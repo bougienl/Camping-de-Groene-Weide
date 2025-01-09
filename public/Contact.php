bf15f5ea8dcc6009b3a8b52bda23faaa1adb0e8c
bougienl <izasuleiman@gmail.com>
2025-01-09 13:22:02 +0100

    9/1/2025

--- public/Contact.php
+++ public/Contact.php
@@ -57,8 +57,19 @@ $conn->close();
         <h1>Neem contact op met ons</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
     </div>
-    <div></div>
     <div class="brownie">
+        <svg class="back" width="613" height="515" viewBox="0 0 613 515" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="645" height="515" fill="#AA9956"/>
+        </svg>
+        <svg class="cookie" xmlns="http://www.w3.org/2000/svg" width="555" height="555" viewBox="0 0 686 693" fill="none">
+            <path d="M549.554 622.222C398.674 737.257 183.971 707.057 70 554.768C-43.9707 402.48 -14.0501 185.772 136.83 70.7377C287.709 -44.2969 502.413 -14.097 616.384 138.191C730.354 290.479 700.434 507.187 549.554 622.222Z" fill="url(#paint0_linear_867_937)"/>
+            <defs>
+                <linearGradient id="paint0_linear_867_937" x1="573.544" y1="90.7393" x2="23.4813" y2="502.4" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#AA9956"/>
+                <stop offset="1" stop-color="#C0AC71"/>
+                </linearGradient>
+            </defs>
+        </svg>
         <div class="adress">
             <h1>Boerencamping<br>
                 "De Groene Weide"</h1>
@@ -66,7 +77,9 @@ $conn->close();
                 <object data="../assets/pin-1.svg" style="margin-bottom: -0.5rem;"></object>Balloo 26 9458TA,Balloo, Drenthe<br>
                 <object data="../assets/mail.svg" style="margin-bottom: -0.75rem;"></object>Bertdebeste@gmail.com</p>
         </div>
+
     </div>
+
 </div>
 <div class="form-container">
     <form action="submit.php" method="POST">
@@ -102,15 +115,19 @@ $conn->close();
         </div>
     </form>
 </div>
-<div class="route">
-    <h1>
-        Route
-    </h1>
-    <p style="color: #8DB411">Met de auto</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
-    <p style="color: #8DB411">Met de trein/bus</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
+<div class="mapsroutebox">
+    <div class="maps">
+        <img src="../assets/location.png">
+    </div>
+    <div class="route">
+        <h1>
+            Route
+        </h1>
+        <p style="color: #8DB411">Met de auto</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
+        <p style="color: #8DB411">Met de trein/bus</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
+    </div>
 </div>
-
